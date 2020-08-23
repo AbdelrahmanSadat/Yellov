2da26747f199cddd37e9a325ae9145af1856b2aa
Abdelrahman Sadat <abdelrahmansadat1100001@gmail.com>
2020-08-23 11:20:39 +0200

    - feat: created a bg overlay component

---
+++ components/BGOverlay.js
@@ -0,0 +1,39 @@
+// TODO: Make flexible (accepts different colors)
+
+// * Takes a layerColor Prop (a string of css color)
+
+import React from "react";
+
+import { Box, Grid } from "@material-ui/core";
+
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+
+
+
+// * CSS Styles Here
+  const useStyles = makeStyles((theme) => ({
+    bgOverlay: {
+      // * sets a default color if there's none passed to props
+      backgroundColor: "rgba(20, 20, 20, 0.7)",
+      position: "relative",
+      top: 0,
+      left: 0,
+      width: "100%",
+      height: "100%",
+    },
+  }));
+
+export default function BGOverlay(props) {
+  const classes = useStyles();
+  const theme = useTheme();
+
+  return (
+    //   <Box className={classes.bgOverlay} style={{backgroundColor: props.layerColor? props.layerColor:undefined}}>
+    //       {props.children}
+    // </Box>
+
+    <Grid container {...props} className={classes.bgOverlay} style={{backgroundColor: props.layerColor? props.layerColor:undefined}}>
+          {props.children}
+    </Grid>
+  );
+}
