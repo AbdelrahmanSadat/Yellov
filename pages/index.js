3d26d190033c4af1422fd5d1bab677e4d7ad7c52
Abdelrahman Sadat <abdelrahmansadat1100001@gmail.com>
2020-08-23 11:20:51 +0200

    - feat: layout of main section

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,19 @@
 import React, { useEffect, useState } from "react";
 
+import BGOverlay from "../components/BGOverlay";
+
 import {
   // sizing,
   // height,
   // boxSizing,
-  // typography,
-  // fontWeight,
+  typography,
+  fontWeight,
 } from "@material-ui/system";
 
-import {
-  
-} from "@material-ui/core";
+import { Grid, Typography } from "@material-ui/core";
 
 import { makeStyles, useTheme } from "@material-ui/core/styles";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
-
 
 // import Flags from "country-flag-icons/react/3x2";
 
@@ -27,13 +26,41 @@ import useMediaQuery from "@material-ui/core/useMediaQuery";
 //   scroller,
 // } from "react-scroll";
 
-
 // * CSS Styles Here
-const useStyles = makeStyles((theme) => ({}));
+const useStyles = makeStyles((theme) => ({
+  backgroundImage: {
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    backgroundAttachment: "fixed",
+  },
+  section01: {
+    backgroundImage: "url(womanLaptopBeach-lg.jpg)",
+    minHeight: "100vh",
+    minWidth: "fit-content",
+    color: "white",
+  },
+}));
 
 export default function HomePage() {
   const classes = useStyles();
   const theme = useTheme();
 
-  return <div>Hello</div>;
+  return (
+    <Grid container>
+      <Grid
+        container
+        className={`${classes.section01} ${classes.backgroundImage}`}
+        alignItems="center"
+      >
+        <BGOverlay alignItems="center" layerColor="rgba(20, 20, 20, 0.7)">
+            <Grid item xs={12}>
+              <Typography variant="h1" align="center" color="inherit">
+                Yellov
+              </Typography>
+            </Grid>
+        </BGOverlay>
+      </Grid>
+    </Grid>
+  );
 }
